da312bb79a3c58cce40871a7b3165ffd18ed9837
masudnaeeni <md.naeeni@gmail.com>
2026-08-28 19:39:15 +0330

    وضعیت اولیه ماژول

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'school students',
+    'version' : '17.0.0.12',
+    'summary': 'school students',
+    'sequence': 1,
+    'description': """""",
+    'category': 'شخصی/student',
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+
+
+    'application': True,
+    'license': 'LGPL-3',
+}
---
+++ model/school_student.py
@@ -0,0 +1,23 @@
+from odoo import models, fields
+
+class SchoolStudent(models.Model):
+    _name = 'school.student'
+    _description = 'School Student'
+
+    first_name = fields.Char(required=True,string ="نام" , required=True)
+    last_name = fields.Char(required=True, string = "نام خانوادگی",required=True)
+    birth_date = fields.Date(string = "تاریخ تولد",required=True, default=fields.Date.today())
+    phone = fields.Char(required=True, string = "شماره تلفن")
+    email = fields.Char(string = "ایمیل")
+    student_code = fields.Char(string = "شماره دانش آموزی", required=True)
+    average = fields.Float(string = "معدل")
+    is_active = fields.Boolean(string = "فعال")
+    grade = fields.Selection([('1','اول'), ('2','دوم'), ('3','سوم'), ('4','چهارم'), ('5','پنجم'), ('6','ششم')], string = "پایه")
+    address = fields.Text(string = "آدرس")
+    description = fields.Text(string = "توضیحات")
+    gender = fields.Selection([('male','مرد'),('female','زن')], string = "جنسیت")
+
+
+
+
+
